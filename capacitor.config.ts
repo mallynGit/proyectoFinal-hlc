b2810823290ab72390cc813cfc47dd9ddfb1e328
mallynGit <miguelitorruiz@gmail.com>
2024-03-04 03:37:25 +0100

    fixed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'proyecto-final',
+  appId: 'iessoterohernandez.hlc.proyectofinal',
+  appName: 'ConstructApp',
   webDir: 'www',
   server: {
     androidScheme: 'https'
